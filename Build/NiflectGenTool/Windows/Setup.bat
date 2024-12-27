c807e0ea4713f3a7ff66da33038e80abbca5bc5d
sainimu78 <393584307@qq.com>
2024-12-27 14:26:17 +0800

    hfs Windows 构建流程

---
+++ Build/NiflectGenTool/Windows/Setup.bat
@@ -0,0 +1,44 @@
+@echo off
+setlocal
+
+set ProjectName=NiflectGenTool
+set ToolsetAndArch=%1
+set ToolDirPath=%cd%
+set path=%ToolDirPath%;%path%
+
+set UserName=WishingContributor
+set Password=1
+::begin, FTP
+::set SrcRootDirPath=ftp://192.168.245.158/Storage/ThirdParty/llvm-project
+::end
+
+set StorageDirPath=http://192.168.245.158/sainimu78_Storage/
+set SrcRootDirPath=%StorageDirPath%/ThirdParty/llvm-project
+set DstRootDirPath=%cd%\..\..\..\ThirdParty\libclang
+set ZipFileName=llvm-project_17_0_6.zip
+set DstZipFilePath=%DstRootDirPath%\%ZipFileName%
+set DstExtractedDirPath=%DstRootDirPath%\llvm-project
+set Dst7zExeFilePath=%ToolDirPath%\7za.exe
+set SrcBinPathDebug=%DstExtractedDirPath%\build\Windows\x64\Debug\bin
+set DstBinPathDebug=%cd%\%ToolsetAndArch%\Debug\%ProjectName%
+set SrcBinPathRelease=%DstExtractedDirPath%\build\Windows\x64\Release\bin
+set DstBinPathRelease=%cd%\%ToolsetAndArch%\Release\%ProjectName%
+
+curl -u %UserName%:%Password% -L -o "%Dst7zExeFilePath%" "%StorageDirPath%/Tool/Windows/7za.exe"
+
+::if not exist "%DstZipFilePath%" (
+::wget -np -nH --cut-dirs=3 -O "%DstZipFilePath%" "%SrcRootDirPath%/%ZipFileName%"
+curl -u %UserName%:%Password% -L -o "%DstZipFilePath%" "%SrcRootDirPath%/%ZipFileName%"
+::)
+
+rmdir /s /q "%DstExtractedDirPath%"
+7za x "%DstZipFilePath%" -o"%DstRootDirPath%"
+del "%DstZipFilePath%"
+del "%Dst7zExeFilePath%"
+
+xcopy /E /y "%SrcBinPathDebug%\libclang.dll" "%DstBinPathDebug%\"
+xcopy /E /y "%SrcBinPathDebug%\libclang.pdb" "%DstBinPathDebug%\"
+
+xcopy /E /y "%SrcBinPathRelease%\libclang.dll" "%DstBinPathRelease%\"
+
+endlocal
