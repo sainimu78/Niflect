cf99260400c29545b5f383c2c379a1de14d9b729
fantangshan_180503 <393584307@qq.com>
2024-12-29 23:08:50 +0800

    Deploy 名称改回 Release

--- Build/Niflect/Windows/Generate.bat
+++ Build/Niflect/Windows/Generate.bat
@@ -1,5 +1,5 @@
 @echo off
 mkdir DefaultBuild
 cd DefaultBuild
-cmake ..\..\..\..\Project\Niflect -DCMAKE_INSTALL_PREFIX=Installed -DSETUP=OFF -DDEPLOY=OFF
+cmake ..\..\..\..\Project\Niflect -DCMAKE_INSTALL_PREFIX=Installed -DPROJECT_SETUP=OFF -DPROJECT_RELEASE=OFF
 cd ..
--- Build/Niflect/Windows/Release.bat
+++ Build/Niflect/Windows/Release.bat
@@ -1,5 +1,5 @@
 @echo off
 mkdir DefaultBuild
 cd DefaultBuild
-cmake ..\..\..\..\Project\Niflect -DCMAKE_INSTALL_PREFIX=Installed -DSETUP=OFF -DDEPLOY=ON
+cmake ..\..\..\..\Project\Niflect -DCMAKE_INSTALL_PREFIX=Installed -DPROJECT_SETUP=OFF -DPROJECT_RELEASE=ON
 cd ..
