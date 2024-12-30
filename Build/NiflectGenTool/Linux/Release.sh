0a303f9318924c4d32a37366047fd59b2ed69aa0
sainimu78 <393584307@qq.com>
2024-12-31 00:23:10 +0800

    改写 NiflectGenTool

---
+++ Build/NiflectGenTool/Linux/Release.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+ProjectName=NiflectGenTool
+BuildDirPathDebug=./DefaultBuild/Debug
+BuildDirPathRelease=./DefaultBuild/Release
+InstallPrefix=../Installed
+Toolset="Unix Makefiles"
+OldDirPath=$(pwd)
+
+cd $BuildDirPathDebug
+cmake ../../../../../Project/$ProjectName -G "$Toolset" -DCMAKE_INSTALL_PREFIX="$InstallPrefix" -DPROJECT_SETUP=OFF -DPROJECT_RELEASE=ON -DCMAKE_BUILD_TYPE=Debug
+cd $OldDirPath
+
+cd $BuildDirPathRelease
+cmake ../../../../../Project/$ProjectName -G "$Toolset" -DCMAKE_INSTALL_PREFIX="$InstallPrefix" -DPROJECT_SETUP=OFF -DPROJECT_RELEASE=ON -DCMAKE_BUILD_TYPE=Release
+cd $OldDirPath
