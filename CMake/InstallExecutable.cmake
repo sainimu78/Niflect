cbae7299bb16c9c3bcc445b1e84e077671604255
fantangshan_180503 <393584307@qq.com>
2024-12-31 02:24:20 +0800

    修复 Install 未一并安装 lib

--- CMake/InstallExecutable.cmake
+++ CMake/InstallExecutable.cmake
@@ -1,13 +1,14 @@
-install(TARGETS ${ModuleName}
-	RUNTIME DESTINATION "${ProjectInstallingTargetDirPath}/${BinDirName}"
-)
 if(WIN32)
+	install(TARGETS ${ModuleName}
+		RUNTIME DESTINATION "${ProjectInstallingTargetDirPath}/${BinDirName}"
+	)
 	install(FILES "$<TARGET_FILE_DIR:${ModuleName}>/${ModuleName}.pdb"
 		DESTINATION "${ProjectInstallingTargetDirPath}/${BinDirName}"
 		CONFIGURATIONS Debug
 	)
 else()
 	install(TARGETS ${ModuleName}
+		RUNTIME DESTINATION "${ProjectInstallingTargetDirPath}/${BinDirName}"
 		LIBRARY DESTINATION "${ProjectInstallingTargetDirPath}/${LibDirName}"
 	)
 endif()
