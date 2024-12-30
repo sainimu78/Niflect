b3f80cdaef11c822c3a39eff1e8211bf1d096d6c
fantangshan_180503 <393584307@qq.com>
2024-12-31 03:18:23 +0800

    改为整理目录覆盖与安装

--- ThirdParty/libclang/CMakeInclude/Shared.cmake
+++ ThirdParty/libclang/CMakeInclude/Shared.cmake
@@ -4,6 +4,10 @@ set(LibRootDirPath ${ThirdPartyLibDirPath}/llvm-project)
 set(LibTargetName ${LibName}_${ModuleName})
 
 set(LibPlatformArchDirPath ${LibRootDirPath}/build/${ProjectPlatform}/${ProjectArch})
+set(BinDirPathDebug ${LibPlatformArchDirPath}/Debug/${BinDirName})
+set(BinDirPathRelease ${LibPlatformArchDirPath}/Release/${BinDirName})
+set(LibDirPathDebug ${LibPlatformArchDirPath}/Debug/${LibDirName})
+set(LibDirPathRelease ${LibPlatformArchDirPath}/Release/${LibDirName})
 set(BinFilePathPrefixDebug ${LibPlatformArchDirPath}/Debug/${BinDirName}/${LibName})
 set(BinFilePathPrefixRelease ${LibPlatformArchDirPath}/Release/${BinDirName}/${LibName})
 set(LibFilePathPrefixDebug ${LibPlatformArchDirPath}/Debug/${LibDirName}/${LibName})
@@ -24,26 +28,61 @@ set(SrcAddrZipFilePath ${StorageAddrPath}/ThirdParty/llvm-project/${ProjectPlatf
 set(DstDownloadedFilePath ${ThirdPartyLibDirPath}/${ZipFileName})
 if(PROJECT_SETUP OR NOT EXISTS "${LibRootDirPath}")
 	download_zip_replace_dir(${SrcAddrZipFilePath} ${DstDownloadedFilePath} ${LibRootDirPath})
-	file(COPY "${LinkingFilePathDebug}" DESTINATION "${CMAKE_LIBRARY_OUTPUT_DIRECTORY_DEBUG}")
-	file(COPY "${LinkingFilePathRelease}" DESTINATION "${CMAKE_LIBRARY_OUTPUT_DIRECTORY_RELEASE}")
-	if(WIN32)
-		file(COPY "${BinFilePathDebug}" DESTINATION "${CMAKE_RUNTIME_OUTPUT_DIRECTORY_DEBUG}")
-		file(COPY "${BinFilePathRelease}" DESTINATION "${CMAKE_RUNTIME_OUTPUT_DIRECTORY_RELEASE}")
-		file(COPY "${PdbFilePath}" DESTINATION "${CMAKE_RUNTIME_OUTPUT_DIRECTORY_DEBUG}")
-	endif()
+	#file(COPY "${LinkingFilePathDebug}" DESTINATION "${CMAKE_LIBRARY_OUTPUT_DIRECTORY_DEBUG}")
+	#file(COPY "${LinkingFilePathRelease}" DESTINATION "${CMAKE_LIBRARY_OUTPUT_DIRECTORY_RELEASE}")
+	#if(WIN32)
+	#	file(COPY "${BinFilePathDebug}" DESTINATION "${CMAKE_RUNTIME_OUTPUT_DIRECTORY_DEBUG}")
+	#	file(COPY "${BinFilePathRelease}" DESTINATION "${CMAKE_RUNTIME_OUTPUT_DIRECTORY_RELEASE}")
+	#	file(COPY "${PdbFilePath}" DESTINATION "${CMAKE_RUNTIME_OUTPUT_DIRECTORY_DEBUG}")
+	#endif()
+	
+	#file(COPY "${BinDirPathDebug}/" DESTINATION "${CMAKE_RUNTIME_OUTPUT_DIRECTORY_DEBUG}/")
+	#file(COPY "${BinDirPathRelease}/" DESTINATION "${CMAKE_RUNTIME_OUTPUT_DIRECTORY_RELEASE}/")
+	#file(COPY "${LibDirPathDebug}/" DESTINATION "${CMAKE_LIBRARY_OUTPUT_DIRECTORY_DEBUG}/")
+	#file(COPY "${LibDirPathRelease}/" DESTINATION "${CMAKE_LIBRARY_OUTPUT_DIRECTORY_RELEASE}/")
+	
+	file(COPY "${LibPlatformArchDirPath}/" DESTINATION "${ProjectPlatformArchDirPath}/")
+	
 endif()
 
 if(WIN32)
-	install(FILES "${BinFilePathDebug}" DESTINATION "${ProjectInstallingTargetDirPathDebug}/${BinDirName}" CONFIGURATIONS Debug)
-	install(FILES "${BinFilePathRelease}" DESTINATION "${ProjectInstallingTargetDirPathRelease}/${BinDirName}" CONFIGURATIONS Release)
-	install(FILES "${PdbFilePath}"
-		DESTINATION "${ProjectInstallingTargetDirPath}/${BinDirName}"
+	install(DIRECTORY "${BinDirPathDebug}/"
+		DESTINATION "${ProjectInstallingTargetDirPathDebug}/${BinDirName}/"
 		CONFIGURATIONS Debug
+		#FILES_MATCHING PATTERN "*.so"
+		USE_SOURCE_PERMISSIONS
+	)
+	install(DIRECTORY "${BinDirPathRelease}/"
+		DESTINATION "${ProjectInstallingTargetDirPathRelease}/${BinDirName}/"
+		CONFIGURATIONS Release
+		#FILES_MATCHING PATTERN "*.so"
+		USE_SOURCE_PERMISSIONS
 	)
 else()
-	install(FILES "${LinkingFilePathDebug}" DESTINATION "${ProjectInstallingTargetDirPathDebug}/${LibDirName}" CONFIGURATIONS Debug)
-	install(FILES "${LinkingFilePathRelease}" DESTINATION "${ProjectInstallingTargetDirPathRelease}/${LibDirName}" CONFIGURATIONS Release)
+	install(DIRECTORY "${LibDirPathDebug}/"
+		DESTINATION "${ProjectInstallingTargetDirPathDebug}/${LibDirName}/"
+		CONFIGURATIONS Debug
+		#FILES_MATCHING PATTERN "*.so"
+		USE_SOURCE_PERMISSIONS
+	)
+	install(DIRECTORY "${LibDirPathRelease}/"
+		DESTINATION "${ProjectInstallingTargetDirPathRelease}/${LibDirName}/"
+		CONFIGURATIONS Release
+		#FILES_MATCHING PATTERN "*.so"
+		USE_SOURCE_PERMISSIONS
+	)
 endif()
+#if(WIN32)
+#	install(FILES "${BinFilePathDebug}" DESTINATION "${ProjectInstallingTargetDirPathDebug}/${BinDirName}" CONFIGURATIONS Debug)
+#	install(FILES "${BinFilePathRelease}" DESTINATION "${ProjectInstallingTargetDirPathRelease}/${BinDirName}" CONFIGURATIONS Release)
+#	install(FILES "${PdbFilePath}"
+#		DESTINATION "${ProjectInstallingTargetDirPath}/${BinDirName}"
+#		CONFIGURATIONS Debug
+#	)
+#else()
+#	install(FILES "${LinkingFilePathDebug}" DESTINATION "${ProjectInstallingTargetDirPathDebug}/${LibDirName}" CONFIGURATIONS Debug)
+#	install(FILES "${LinkingFilePathRelease}" DESTINATION "${ProjectInstallingTargetDirPathRelease}/${LibDirName}" CONFIGURATIONS Release)
+#endif()
 	
 add_library(${LibTargetName} SHARED IMPORTED)
 
