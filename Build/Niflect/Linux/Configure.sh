f8cee549966994a26e1cbfe66800f406a7af113a
sainimu78 <393584307@qq.com>
2024-12-30 22:43:09 +0800

    从 MinGW 的 Build 改写

--- Build/Niflect/Linux/Configure.sh
+++ Build/Niflect/Linux/Configure.sh
@@ -1,11 +1,25 @@
 #!/bin/bash
-mkdir DefaultBuild
-cd DefaultBuild
-del CMakeCache.txt
-cmake ../../../../../Project/Niflect -DCMAKE_INSTALL_PREFIX=../Installed -DPROJECT_SETUP=OFF -DPROJECT_RELEASE=OFF -DCMAKE_BUILD_TYPE=Debug
-cmake ../../../../../Project/Niflect -DCMAKE_INSTALL_PREFIX=../Installed -DPROJECT_SETUP=OFF -DPROJECT_RELEASE=OFF -DCMAKE_BUILD_TYPE=Release
-if %ERRORLEVEL% neq 0 (
-    echo "### cmake generating failed ###"
-    pause
-)
-cd ..
+
+Toolset="Unix Makefiles"
+BuildDirPathDebug=./DefaultBuild/Debug
+BuildDirPathRelease=./DefaultBuild/Release
+InstallPrefix=../Installed
+
+OldDirPath=$(pwd)
+mkdir -p $BuildDirPathDebug
+cd $BuildDirPathDebug
+rm -f "./CMakeCache.txt"
+cmake ../../../../../Project/Niflect -G "$Toolset" -DCMAKE_INSTALL_PREFIX="$InstallPrefix" -DPROJECT_SETUP=OFF -DPROJECT_RELEASE=OFF -DCMAKE_BUILD_TYPE=Debug
+if [ $? -ne 0 ]; then
+    echo "CMake configuration failed for Debug build."
+fi
+cd $OldDirPath
+
+mkdir -p $BuildDirPathRelease
+cd $BuildDirPathRelease
+rm -f "./CMakeCache.txt"
+cmake ../../../../../Project/Niflect -G "$Toolset" -DCMAKE_INSTALL_PREFIX="$InstallPrefix" -DPROJECT_SETUP=OFF -DPROJECT_RELEASE=OFF -DCMAKE_BUILD_TYPE=Release
+if [ $? -ne 0 ]; then
+    echo "CMake configuration failed for Debug build."
+fi
+cd $OldDirPath
