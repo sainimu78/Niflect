f5d4d6826c34e524a4c7559f58674585d71f88fc
sainimu78 <393584307@qq.com>
2024-12-31 13:48:15 +0800

    修复重复打包

--- Build/NiflectGenTool/Linux/Release.sh
+++ Build/NiflectGenTool/Linux/Release.sh
@@ -6,9 +6,9 @@ InstallPrefix=../Installed
 Toolset="Unix Makefiles"
 OldDirPath=$(pwd)
 
-cd $BuildDirPathDebug
-cmake ../../../../../Project/$ProjectName -G "$Toolset" -DCMAKE_INSTALL_PREFIX="$InstallPrefix" -DPROJECT_SETUP=OFF -DPROJECT_RELEASE=ON -DCMAKE_BUILD_TYPE=Debug
-cd $OldDirPath
+#cd $BuildDirPathDebug
+#cmake ../../../../../Project/$ProjectName -G "$Toolset" -DCMAKE_INSTALL_PREFIX="$InstallPrefix" -DPROJECT_SETUP=OFF -DPROJECT_RELEASE=ON -DCMAKE_BUILD_TYPE=Debug
+#cd $OldDirPath
 
 cd $BuildDirPathRelease
 cmake ../../../../../Project/$ProjectName -G "$Toolset" -DCMAKE_INSTALL_PREFIX="$InstallPrefix" -DPROJECT_SETUP=OFF -DPROJECT_RELEASE=ON -DCMAKE_BUILD_TYPE=Release
