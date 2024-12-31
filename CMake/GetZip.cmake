b47cb30cae5f7c74ce9131529dbb996c6f8ac1c7
fantangshan_180503 <393584307@qq.com>
2024-12-31 14:47:43 +0800

    改用7z

--- CMake/GetZip.cmake
+++ CMake/GetZip.cmake
@@ -1,11 +1,26 @@
 if(WIN32)
-	set(ExeName7z 7za.exe)
-	set(ExeFilePath7z ${ProjectTempDirPath}/${ExeName7z})
-	if(NOT EXISTS "${ExeFilePath7z}")
-		set(SrcFilePath ${StorageAddrPath}/Tool/${ProjectPlatform}/7z/${ExeName7z})
-		set(DstFilePath ${ExeFilePath7z})
+	set(FileName 7z.exe)
+	set(FilePathDownloaded ${ProjectTempDirPath}/${FileName})
+	set(ExeFilePath7z ${FilePathDownloaded})
+	if(NOT EXISTS "${FilePathDownloaded}")
+		set(SrcFilePath ${StorageAddrPath}/Tool/${ProjectPlatform}/7z/${FileName})
+		set(DstFilePath ${FilePathDownloaded})
 		message(STATUS "Downloading ${DstFilePath} from ${SrcFilePath}")
-		file(DOWNLOAD ${SrcFilePath} ${DstFilePath}
+		file(DOWNLOAD "${SrcFilePath}" "${DstFilePath}"
+			 STATUS status
+			 #SHOW_PROGRESS
+		)
+		if(status EQUAL 0)
+			message(STATUS "File downloaded successfully.")
+		else()
+			message(FATAL_ERROR "File download failed.")
+		endif()
+		set(FileName 7z.dll)
+		set(FilePathDownloaded ${ProjectTempDirPath}/${FileName})
+		set(SrcFilePath ${StorageAddrPath}/Tool/${ProjectPlatform}/7z/${FileName})
+		set(DstFilePath ${FilePathDownloaded})
+		message(STATUS "Downloading ${DstFilePath} from ${SrcFilePath}")
+		file(DOWNLOAD "${SrcFilePath}" "${DstFilePath}"
 			 STATUS status
 			 #SHOW_PROGRESS
 		)
