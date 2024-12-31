ddd4cfac002a989d7e4d39cb1519ad00c13862aa
fantangshan_180503 <393584307@qq.com>
2025-01-01 02:27:32 +0800

    调整变量顺序, 准备规范 cmake 代码风格

--- ThirdParty/libclang/CMakeInclude/Shared.cmake
+++ ThirdParty/libclang/CMakeInclude/Shared.cmake
@@ -1,61 +1,31 @@
 set(LibName libclang)
 set(ThirdPartyLibDirPath ${RootThirdPartyDirPath}/${LibName})
 set(LibRootDirPath ${ThirdPartyLibDirPath}/llvm-project)
-set(LibTargetName ${LibName}_${ModuleName})
+set(ZipFileName llvm-project_17_0_6.zip)
+set(SrcAddrZipFilePath ${StorageAddrPath}/ThirdParty/llvm-project/${ProjectPlatform}/${ZipFileName})
 
 set(LibPlatformArchDirPath ${LibRootDirPath}/build/${ProjectPlatform}/${ProjectArch})
-set(BinDirPathDebug ${LibPlatformArchDirPath}/Debug/${BinDirName})
-set(BinDirPathRelease ${LibPlatformArchDirPath}/Release/${BinDirName})
 set(LibDirPathDebug ${LibPlatformArchDirPath}/Debug/${LibDirName})
 set(LibDirPathRelease ${LibPlatformArchDirPath}/Release/${LibDirName})
-#set(BinFilePathPrefixDebug ${LibPlatformArchDirPath}/Debug/${BinDirName}/${LibName})
-#set(BinFilePathPrefixRelease ${LibPlatformArchDirPath}/Release/${BinDirName}/${LibName})
-set(LibFilePathPrefixDebug ${LibPlatformArchDirPath}/Debug/${LibDirName}/${LibName})
-set(LibFilePathPrefixRelease ${LibPlatformArchDirPath}/Release/${LibDirName}/${LibName})
-#set(BinFilePathDebug ${BinFilePathPrefixDebug}${ProjectSharedLibPostfix})
-#set(BinFilePathRelease ${BinFilePathPrefixRelease}${ProjectSharedLibPostfix})
+
+set(LibFilePathPrefixDebug ${LibDirPathDebug}/${LibName})
+set(LibFilePathPrefixRelease ${LibDirPathRelease}/${LibName})
 if(WIN32)
 	set(LinkingFilePathDebug ${LibFilePathPrefixDebug}${ProjectStaticLibPostfix})
 	set(LinkingFilePathRelease ${LibFilePathPrefixRelease}${ProjectStaticLibPostfix})
-	#set(PdbFilePath ${BinFilePathPrefixDebug}.pdb)
 else()
 	set(LinkingFilePathDebug ${LibFilePathPrefixDebug}${ProjectSharedLibPostfix})
 	set(LinkingFilePathRelease ${LibFilePathPrefixRelease}${ProjectSharedLibPostfix})
 endif()
 	
-set(ZipFileName llvm-project_17_0_6.zip)
-set(SrcAddrZipFilePath ${StorageAddrPath}/ThirdParty/llvm-project/${ProjectPlatform}/${ZipFileName})
 set(DstDownloadedFilePath ${ThirdPartyLibDirPath}/${ZipFileName})
 if(PROJECT_SETUP OR NOT EXISTS "${LibRootDirPath}")
 	download_zip_replace_dir(${SrcAddrZipFilePath} ${DstDownloadedFilePath} ${LibRootDirPath})
-	#file(COPY "${LinkingFilePathDebug}" DESTINATION "${CMAKE_LIBRARY_OUTPUT_DIRECTORY_DEBUG}")
-	#file(COPY "${LinkingFilePathRelease}" DESTINATION "${CMAKE_LIBRARY_OUTPUT_DIRECTORY_RELEASE}")
-	#if(WIN32)
-	#	file(COPY "${BinFilePathDebug}" DESTINATION "${CMAKE_RUNTIME_OUTPUT_DIRECTORY_DEBUG}")
-	#	file(COPY "${BinFilePathRelease}" DESTINATION "${CMAKE_RUNTIME_OUTPUT_DIRECTORY_RELEASE}")
-	#	file(COPY "${PdbFilePath}" DESTINATION "${CMAKE_RUNTIME_OUTPUT_DIRECTORY_DEBUG}")
-	#endif()
-	
-	#file(COPY "${BinDirPathDebug}/" DESTINATION "${CMAKE_RUNTIME_OUTPUT_DIRECTORY_DEBUG}/")
-	#file(COPY "${BinDirPathRelease}/" DESTINATION "${CMAKE_RUNTIME_OUTPUT_DIRECTORY_RELEASE}/")
-	#file(COPY "${LibDirPathDebug}/" DESTINATION "${CMAKE_LIBRARY_OUTPUT_DIRECTORY_DEBUG}/")
-	#file(COPY "${LibDirPathRelease}/" DESTINATION "${CMAKE_LIBRARY_OUTPUT_DIRECTORY_RELEASE}/")
-	
 	file(COPY "${LibPlatformArchDirPath}/" DESTINATION "${ProjectPlatformArchDirPath}/")
-	
 endif()
 
-#if(WIN32)
-#	install(FILES "${BinFilePathDebug}" DESTINATION "${ProjectInstallingTargetDirPathDebug}/${BinDirName}" CONFIGURATIONS Debug)
-#	install(FILES "${BinFilePathRelease}" DESTINATION "${ProjectInstallingTargetDirPathRelease}/${BinDirName}" CONFIGURATIONS Release)
-#	install(FILES "${PdbFilePath}"
-#		DESTINATION "${ProjectInstallingTargetDirPath}/${BinDirName}"
-#		CONFIGURATIONS Debug
-#	)
-#else()
-#	install(FILES "${LinkingFilePathDebug}" DESTINATION "${ProjectInstallingTargetDirPathDebug}/${LibDirName}" CONFIGURATIONS Debug)
-#	install(FILES "${LinkingFilePathRelease}" DESTINATION "${ProjectInstallingTargetDirPathRelease}/${LibDirName}" CONFIGURATIONS Release)
-#endif()
+set(BinDirPathDebug ${LibPlatformArchDirPath}/Debug/${BinDirName})
+set(BinDirPathRelease ${LibPlatformArchDirPath}/Release/${BinDirName})
 
 # 区分不同 Configuration 是为支持仅以 Release 版本安装与发布
 if(WIN32)
@@ -86,9 +56,10 @@ else()
 	)
 endif()
 
-add_library(${LibTargetName} SHARED IMPORTED)
-
 target_include_directories(${ModuleName} PRIVATE "${LibRootDirPath}/clang/include")
+
+set(LibTargetName ${LibName}_${ModuleName})
+add_library(${LibTargetName} SHARED IMPORTED)
 
 if(x64)
 	if(WIN32)
