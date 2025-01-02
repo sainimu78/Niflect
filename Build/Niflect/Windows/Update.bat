614ccf3927a632daa454838bd312d73414245222
sainimu78 <393584307@qq.com>
2025-01-02 22:09:02 +0800

    合并复用 cmake 执行脚本

--- Build/Niflect/Windows/Update.bat
+++ Build/Niflect/Windows/Update.bat
@@ -2,4 +2,8 @@
 mkdir DefaultBuild
 cd DefaultBuild
 cmake ..\..\..\..\Project\Niflect -DCMAKE_INSTALL_PREFIX=Installed -DPROJECT_SETUP=OFF -DPROJECT_RELEASE=OFF
+if %ERRORLEVEL% neq 0 (
+    echo "### cmake generating failed ###"
+    pause
+)
 cd ..
