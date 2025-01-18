ed1460f2b4286ca9a80515078ed8ee0c17e3499c
sainimu78 <393584307@qq.com>
2025-01-19 01:12:41 +0800

    修复测试

--- Source/NiflectGen/CodeWriter/ModuleReg/ModuleRegCode.cpp
+++ Source/NiflectGen/CodeWriter/ModuleReg/ModuleRegCode.cpp
@@ -73,7 +73,7 @@ namespace NiflectGen
 			info.m_moduleName = "Engine";
 			info.m_genOutputDirPath = CONCAT_HARDCODED_STRINGS_2(ROOT_TEST_PROJECT_PATH, "/Generated/NiflectGenerated");
 			info.m_toolHeaderSearchPath = CONCAT_HARDCODED_STRINGS_2(ROOT_TEST_INCLUDE_PATH, "/Niflect/include/");
-			info.m_vecModuleHeaderSearchPath2.push_back(CONCAT_HARDCODED_STRINGS_2(ROOT_TEST_INCLUDE_PATH, "/TestEngine/include/"));
+			info.m_vecModuleHeaderSearchPath2.push_back(CONCAT_HARDCODED_STRINGS_2(ROOT_TEST_INCLUDE_PATH, "/NiflectGen/Test/TestEngine/include/"));
 			info.m_vecModuleHeaderSearchPath2.push_back(CONCAT_HARDCODED_STRINGS_2(ROOT_TEST_INCLUDE_PATH, "/"));
 		}
 	}
