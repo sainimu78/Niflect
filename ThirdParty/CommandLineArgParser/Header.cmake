483e4e9221f133ae0ecf0f2fc54cc96100bf7b55
sainimu78 <393584307@qq.com>
2025-01-28 21:26:07 +0800

    修复误认为全局变量未用

--- ThirdParty/CommandLineArgParser/Header.cmake
+++ ThirdParty/CommandLineArgParser/Header.cmake
@@ -1,6 +1,6 @@
 set(ImportedLibName CommandLineArgParser)
-set(ImportedLibRootDirPath ${c_RootThirdPartyDirPath}/${ImportedLibName})
-set(v_UnzippedDirPath ${ImportedLibRootDirPath}/${ImportedLibName})
+set(v_ImportedLibRootDirPath ${c_RootThirdPartyDirPath}/${ImportedLibName})
+set(v_UnzippedDirPath ${v_ImportedLibRootDirPath}/${ImportedLibName})
 set(v_ZipFileName ${ImportedLibName}.zip)
 set(v_SrcAddrZipFilePath ${c_StorageAddrPath}/Release/${c_ProjectPlatform}/${v_ZipFileName})
 list(APPEND v_ListLibIncludeDirPathPrivate ${v_UnzippedDirPath}/include)
