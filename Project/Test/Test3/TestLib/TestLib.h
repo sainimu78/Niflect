0ca0c84c6dd58660af34af5d7aa6fc399330b566
sainimu78 <393584307@qq.com>
2025-03-30 20:54:12 +0800

    构造函数, Method 的测试

--- Project/Test/Test3/TestLib/TestLib.h
+++ Project/Test/Test3/TestLib/TestLib.h
@@ -1,15 +1,103 @@
 #pragma once
 #include "Niflect/NiflectTable.h"
+#include "Niflect/Base/Map.h"
 #include "TestLib_gen.h"
 
+//NIF_T()
+//struct STestBase
+//{
+//
+//};
+//
+//NIF_T()
+//struct STestLib : STestBase
+//{
+//
+//};
+
 NIF_T()
-class CTestLib
+class CTestBase
 {
+	GENERATED_BODY()
+};
+
+NIF_T()
+class CTestLib : public CTestBase
+{
+	GENERATED_BODY()
 public:
 	CTestLib()
 		: m_float_0(0.0f)
+		, m_string_2(NULL)
 	{
 	}
+	//CTestLib(Niflect::TArray<float> a, const Niflect::TArray<bool>& b, Niflect::CString, Niflect::TMap<Niflect::CString, float>)
+	//	: m_float_0(0.0f)
+	//	, m_string_2(NULL)
+	//{
+	//}
+	//NIF_M()
+	//CTestLib(const float& a)
+	//	: m_float_0(0.0f)
+	//	, m_string_2(NULL)
+	//{
+	//}
+	//NIF_M()
+	//Niflect::CString FuncB(
+	//	//Niflect::TMap<Niflect::CString, float> a,
+	//	//Niflect::TArray<Niflect::CString> b,
+	//	//Niflect::CString& c,
+	//	//const Niflect::CString& d,
+	//	//Niflect::CString* e,
+	//	const Niflect::CString* f//,
+	//	//Niflect::TArray<float>& g,
+	//	//Niflect::TArray<float>* h,
+	//	//const Niflect::TArray<float>* i,
+	//	//const Niflect::TArray<Niflect::TMap<int32, Niflect::CString> >* j
+	//)
+	//{
+	//	return "";
+	//}
+	
+private:
+	void Nihao()
+	{
+	}
+
+public:
+	//NIF_M()
+	//void FuncA(Niflect::TArray<float> a, const Niflect::TArray<bool>& b, Niflect::CString, Niflect::TMap<Niflect::CString, float>)
+	//{
+	//}
+	//NIF_M()
+	//Niflect::CString FuncB(Niflect::TArray<float> a, const Niflect::TArray<bool>& b, Niflect::CString, Niflect::TMap<Niflect::CString, float>)
+	//{
+	//	return "";
+	//}
+	//float FuncC(Niflect::TArray<float> a, const Niflect::TArray<bool>& b, Niflect::CString, Niflect::TMap<Niflect::CString, float>)
+	//{
+	//}
+	//const Niflect::CString& FuncD(Niflect::TArray<float> a, const Niflect::TArray<bool>& b, Niflect::CString, Niflect::TMap<Niflect::CString, float>)
+	//{
+	//	static Niflect::CString s;
+	//	return s;
+	//}
+	//void FuncD() const
+	//{
+
+	//}
+	//void FuncE(float*, Niflect::CString*, Niflect::TArray<float>**)
+	//{
+
+	//}
+	//float* FuncF() const
+	//{
+	//	return NULL;
+	//}
+	//Niflect::CString* FuncG()
+	//{
+	//	return NULL;
+	//}
 	void InitForTest()
 	{
 		m_float_0 = 1.23f;
@@ -21,6 +109,10 @@ public:
 	}
 
 public:
+	//NIF_F()
+	//Niflect::TArray<float> m_array_float_1;
+	NIF_F()
+	Niflect::CString* m_string_2;
 	NIF_F()
 	float m_float_0;
 };
--- Project/Test/Test3/TestLib/TestLibAccessorSetting.h
+++ Project/Test/Test3/TestLib/TestLibAccessorSetting.h
@@ -1,1 +1,20 @@
+#pragma once
 #include "Niflect/Default/DefaultAccessorSetting.h"
+
+using namespace NiflectAccessorSetting;
+using namespace RwTree;
+
+class CMyStringPointerAccessor : public Niflect::CAccessor
+{
+protected:
+	virtual bool SaveInstanceImpl(const InstanceType* base, CRwNode* rw) const override
+	{
+		return false;
+	}
+	virtual bool LoadInstanceImpl(InstanceType* base, const CRwNode* rw) const override
+	{
+		return false;
+	}
+};
+
+NIF_AS_A() TSetting<CMyStringPointerAccessor, Niflect::CString*>;
